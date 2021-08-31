fc3438543c6e3d859b604a51060fed92e6b0c569
Tobias Fischer <info@tobiasfischer.info>
2021-09-01 07:05:18 +1000

    Update setup.py

--- rclpy/actions/minimal_action_client/setup.py
+++ rclpy/actions/minimal_action_client/setup.py
@@ -29,7 +29,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'client = ' + package_name + '.client:main',
+            'client_actions = ' + package_name + '.client:main',
             'client_cancel = ' + package_name + '.client_cancel:main',
             'client_not_composable = ' + package_name + '.client_not_composable:main',
             'client_asyncio = ' + package_name + '.client_asyncio:main',
